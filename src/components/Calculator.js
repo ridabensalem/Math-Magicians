c8db1f5eedb08843eea01893342df77a6bf2d770
ridabensalem <ridasbnesalmas@gmail.com>
2023-02-22 09:49:50 +0100

    Chaged some classes in the calculator selctors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,64 +6,64 @@ const Calculator = () => (
 
     <div className="calc-grid">
       <section className="calc-numbers">
-        <button id="btn-ac" className="btn" type="button">
+        <button id="btn-ac" className="button_calculator" type="button">
           AC
         </button>
-        <button id="btn-minus-plus" className="btn" type="button">
+        <button id="btn-minus-plus" className="button_calculator" type="button">
           +/-
         </button>
-        <button id="btn-percentage" className="btn" type="button">
+        <button id="btn-percentage" className="button_calculator" type="button">
           %
         </button>
-        <button id="btn-one" className="btn" type="button">
+        <button id="btn-one" className="button_calculator" type="button">
           1
         </button>
-        <button id="btn-two" className="btn" type="button">
+        <button id="btn-two" className="button_calculator" type="button">
           2
         </button>
-        <button id="btn-three" className="btn" type="button">
+        <button id="btn-three" className="button_calculator" type="button">
           3
         </button>
-        <button id="btn-four" className="btn" type="button">
+        <button id="btn-four" className="button_calculator" type="button">
           4
         </button>
-        <button id="btn-five" className="btn" type="button">
+        <button id="btn-five" className="button_calculator" type="button">
           5
         </button>
-        <button id="btn-six" className="btn" type="button">
+        <button id="btn-six" className="button_calculator" type="button">
           6
         </button>
-        <button id="btn-seven" className="btn" type="button">
+        <button id="btn-seven" className="button_calculator" type="button">
           7
         </button>
-        <button id="btn-eight" className="btn" type="button">
+        <button id="btn-eight" className="button_calculator" type="button">
           8
         </button>
-        <button id="btn-nine" className="btn" type="button">
+        <button id="btn-nine" className="button_calculator" type="button">
           9
         </button>
-        <button id="btn-zero" className="btn" type="button">
+        <button id="btn-zero" className="button_calculator" type="button">
           0
         </button>
-        <button id="btn-period" className="btn" type="button">
+        <button id="btn-period" className="button_calculator" type="button">
           .
         </button>
       </section>
 
       <section className="calc-operators">
-        <button id="btn-four" className="btn" type="button">
+        <button id="btn-four" className="button_calculator" type="button">
           /
         </button>
-        <button id="btn-four" className="btn" type="button">
+        <button id="btn-four" className="button_calculator" type="button">
           *
         </button>
-        <button id="btn-four" className="btn" type="button">
+        <button id="btn-four" className="button_calculator" type="button">
           -
         </button>
-        <button id="btn-four" className="btn" type="button">
+        <button id="btn-four" className="button_calculator" type="button">
           +
         </button>
-        <button id="btn-four" className="btn" type="button">
+        <button id="btn-four" className="button_calculator" type="button">
           =
         </button>
       </section>
