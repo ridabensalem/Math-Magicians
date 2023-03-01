3dcedd69232145d5f8a888b97adec860c03e29b0
ridabensalem <ridasbnesalmas@gmail.com>
2023-03-01 12:19:21 +0100

    Created home , quote , calculator , pages

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -34,77 +34,79 @@ const Calculator = () => {
 
   const { total, next, operation } = state;
   return (
-    <main className="calculator">
-      <header className="calc-output">
-        {total}
-        {' '}
-        {operation}
-        {next}
-      </header>
+    <div className="App">
+      <main className="calculator">
+        <header className="calc-output">
+          {total}
+          {' '}
+          {operation}
+          {next}
+        </header>
 
-      <div className="calc-grid">
-        <section className="calc-numbers">
-          <button id="btn-ac" className="button_calculator" type="button" onClick={() => handleClick('AC')}>
-            AC
-          </button>
-          <button id="btn-minus-plus" className="button_calculator" type="button" onClick={() => handleClick('+/-')}>
-            +/-
-          </button>
-          <button id="btn-percentage" className="button_calculator" type="button" onClick={() => handleClick('%')}>
-            %
-          </button>
-          <Button value="1" onClick={() => handleClick('1')} />
-          <button id="btn-two" className="button_calculator" type="button" onClick={() => handleClick('2')}>
-            2
-          </button>
-          <button id="btn-three" className="button_calculator" type="button" onClick={() => handleClick('3')}>
-            3
-          </button>
-          <button id="btn-four" className="button_calculator" type="button" onClick={() => handleClick('4')}>
-            4
-          </button>
-          <button id="btn-five" className="button_calculator" type="button" onClick={() => handleClick('5')}>
-            5
-          </button>
-          <button id="btn-six" className="button_calculator" type="button" onClick={() => handleClick('6')}>
-            6
-          </button>
-          <button id="btn-seven" className="button_calculator" type="button" onClick={() => handleClick('7')}>
-            7
-          </button>
-          <button id="btn-eight" className="button_calculator" type="button" onClick={() => handleClick('8')}>
-            8
-          </button>
-          <button id="btn-nine" className="button_calculator" type="button" onClick={() => handleClick('9')}>
-            9
-          </button>
-          <button id="btn-zero" className="button_calculator" type="button" onClick={() => handleClick('0')}>
-            0
-          </button>
-          <button id="btn-period" className="button_calculator" type="button" onClick={() => handleClick('.')}>
-            .
-          </button>
-        </section>
+        <div className="calc-grid">
+          <section className="calc-numbers">
+            <button id="btn-ac" className="button_calculator" type="button" onClick={() => handleClick('AC')}>
+              AC
+            </button>
+            <button id="btn-minus-plus" className="button_calculator" type="button" onClick={() => handleClick('+/-')}>
+              +/-
+            </button>
+            <button id="btn-percentage" className="button_calculator" type="button" onClick={() => handleClick('%')}>
+              %
+            </button>
+            <Button value="1" onClick={() => handleClick('1')} />
+            <button id="btn-two" className="button_calculator" type="button" onClick={() => handleClick('2')}>
+              2
+            </button>
+            <button id="btn-three" className="button_calculator" type="button" onClick={() => handleClick('3')}>
+              3
+            </button>
+            <button id="btn-four" className="button_calculator" type="button" onClick={() => handleClick('4')}>
+              4
+            </button>
+            <button id="btn-five" className="button_calculator" type="button" onClick={() => handleClick('5')}>
+              5
+            </button>
+            <button id="btn-six" className="button_calculator" type="button" onClick={() => handleClick('6')}>
+              6
+            </button>
+            <button id="btn-seven" className="button_calculator" type="button" onClick={() => handleClick('7')}>
+              7
+            </button>
+            <button id="btn-eight" className="button_calculator" type="button" onClick={() => handleClick('8')}>
+              8
+            </button>
+            <button id="btn-nine" className="button_calculator" type="button" onClick={() => handleClick('9')}>
+              9
+            </button>
+            <button id="btn-zero" className="button_calculator" type="button" onClick={() => handleClick('0')}>
+              0
+            </button>
+            <button id="btn-period" className="button_calculator" type="button" onClick={() => handleClick('.')}>
+              .
+            </button>
+          </section>
 
-        <section className="calc-operators">
-          <button id="btn-divide" className="button_calculator" type="button" onClick={() => handleClick('/')}>
-            /
-          </button>
-          <button id="btn-multiply" className="button_calculator" type="button" onClick={() => handleClick('x')}>
-            *
-          </button>
-          <button id="btn-subtract" className="button_calculator" type="button" onClick={() => handleClick('-')}>
-            -
-          </button>
-          <button id="btn-add" className="button_calculator" type="button" onClick={() => handleClick('+')}>
-            +
-          </button>
-          <button id="btn-equals" className="button_calculator" type="button" onClick={() => handleEquals()}>
-            =
-          </button>
-        </section>
-      </div>
-    </main>
+          <section className="calc-operators">
+            <button id="btn-divide" className="button_calculator" type="button" onClick={() => handleClick('/')}>
+              /
+            </button>
+            <button id="btn-multiply" className="button_calculator" type="button" onClick={() => handleClick('x')}>
+              *
+            </button>
+            <button id="btn-subtract" className="button_calculator" type="button" onClick={() => handleClick('-')}>
+              -
+            </button>
+            <button id="btn-add" className="button_calculator" type="button" onClick={() => handleClick('+')}>
+              +
+            </button>
+            <button id="btn-equals" className="button_calculator" type="button" onClick={() => handleEquals()}>
+              =
+            </button>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 };
 
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h1 className="header_magician">Welcome to Math Magician</h1>
+    <p>
+      The Math Magician Calculator is a remarkable tool that transcends the boundaries of t
+      <br />
+      raditional arithmetic computation. It possesses the magical ability to effortlessly perform
+      complex mathematical operations wit
+      h lightning speed and precision, leaving users awe-inspired and captivated.
+    </p>
+    <p>
+      The Math Magician Calculator is not only highly efficient but also highly versatile,
+      <br />
+      capable of performing a vast range of mathematical operations with ease.
+      Whether you need to perform basic arithmetic calculations,
+      algebraic equations, trigonometric functions, or statistical analyses,
+      this calculator has got you covered.
+    </p>
+  </>
+);
+export default Home;
